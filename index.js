6a1b41adb67986e7fc1e5fae16c3a1b1a5f1f93d
Ben Manning <bmanning@me.com>
2015-07-17 15:53:28 -0700

    second fix on routes

--- index.js
+++ index.js
@@ -7,16 +7,22 @@ bodyParser  = require('body-parser');
 // using bodyParser middleware
 app.use(bodyParser.urlencoded({extended: true}));
 
+// use for static assets
+app.use(express.static(__dirname + '/public'));
+
 
 
 app.listen(3000, function() {
 	console.log('Server is running.');
 });
 
+// serve js and css files from public folder
+app.use(express.static(__dirname + '/public'));
+
 // routing
 
-app.get("/", function(req,res) {
-	res.send("Index route is working!");
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 app.get("/sessions/1", function(req,res) {
